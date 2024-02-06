071c5881531dabcec7a8ab67bdbc48c7dddedfe6
Miguel Monzo <miguel.monzo@tecsidel.com>
2024-02-06 11:31:00 +0100

    Add hero and monster battle logic

--- TestProject13/Program.cs
+++ TestProject13/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int heroeHealth = 10;
+int monsterHealth = 10;
+
+Random random = new Random();
+do{
+
+    int heroeAttack = random.Next(1, 11);
+    monsterHealth -= heroeAttack;
+    Console.WriteLine($"Heroe attacks monster with {heroeAttack} damage. Monster health is {monsterHealth}");
+
+    if (monsterHealth > 0){
+        int monsterAttack = random.Next(1, 11);
+        heroeHealth -= monsterAttack;
+        Console.WriteLine($"Monster attacks heroe with {monsterAttack} damage. Heroe health is {heroeHealth}");
+    }
+
+}while(heroeHealth > 0 && monsterHealth > 0);
+
+Console.WriteLine(heroeHealth <= 0 ? "Monster wins!" : "Heroe wins!");
---
+++ TestProject14/Program.cs
@@ -0,0 +1,75 @@
+﻿/* string? readResult;
+
+Console.WriteLine("Enter a string:");
+do{
+    readResult = Console.ReadLine();
+}while(readResult == null); */
+
+/* string? readResult;
+bool validEntry = false;
+Console.WriteLine("Enter a string containing at least three characters:");
+
+do{
+    readResult = Console.ReadLine();
+    if (readResult?.Length >= 3)
+    {
+        validEntry = true;
+    }
+    else
+    {
+        Console.WriteLine("You must enter at least three characters!");
+    }
+}while(validEntry == false); */
+
+/* string? readResult;
+bool validNumber = false;
+Console.WriteLine("Enter a number containing at least three characters:");
+
+do{
+    readResult = Console.ReadLine();
+    if (readResult?.Length >= 3)
+    {
+        validNumber = int.TryParse(readResult, out int number);
+    }
+    else
+    {
+        Console.WriteLine("You must enter at least three numbers!");
+    }
+}while(validNumber == false);
+
+Console.WriteLine("You have entered three numbers!"); */
+
+string? readResult;
+/* Console.WriteLine("Enter a number between 5 and 10:");
+do{
+    readResult = Console.ReadLine();
+    if (readResult != null){
+        int number = int.Parse(readResult);
+        if (number >= 5 && number <= 10){
+            Console.WriteLine("Valid number entered!");
+            break;
+        }
+        else{
+            Console.WriteLine("Invalid number entered!");
+            Console.WriteLine("Enter a number between 5 and 10:");
+        }
+    }
+}while(true);
+Console.WriteLine("Your number is between 5 and 10!"); */
+
+Console.WriteLine("Enter one rol name: Admin, Manager, User");
+do{
+    readResult = Console.ReadLine();
+    if (readResult != null){
+        readResult = readResult.Trim().ToLower();
+        if (readResult == "admin" || readResult == "manager" || readResult == "user"){
+            Console.WriteLine("Valid role entered!");
+            break;
+        }
+        else{
+            Console.WriteLine("Invalid role entered!");
+            Console.WriteLine("Enter one rol name: Admin, Manager, User");
+        }
+    }
+}while(true);
+Console.WriteLine("Your role is valid!");
